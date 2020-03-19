fd412a859d6532e038730954e4e4b88f0584968e
Debra Sparr <50783303+dsparr1010@users.noreply.github.com>
2020-03-19 14:17:29 -0500

    start building out back end

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+require("dotenv").config();
+app.use(morgan("dev"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+};
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./build/index.html"));
+});
